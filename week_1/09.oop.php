31d8e60c3542db23bdffca18480785c6c7461b49
Azajul-Karim <azajulkarim2021@gmail.com>
2023-09-15 08:32:58 +0600

    abstract concept added

---
+++ week_1/09.oop.php
@@ -0,0 +1,54 @@
+<?php
+//Abstract class
+
+abstract class Vehicle
+{
+  abstract public function getBaseFare(): int;
+   
+
+  abstract public function getPerkiloFare():int;
+   
+
+    public function getTotal(int $kilo)
+    {
+        return $this->getBaseFare() + ($kilo * $this->getPerkiloFare());
+       
+    }
+}
+
+class Car extends Vehicle
+{
+  public function getBaseFare(): int{
+    return 100;
+  }
+
+  public function getPerkiloFare(): int{
+    return 10;
+  }
+}
+
+class Bike extends Vehicle
+{
+  public function getBaseFare(): int{
+    return 50;
+  }
+
+  public function getPerkiloFare(): int{
+    return 5;
+  }
+}
+
+class CNG extends Vehicle
+{
+  public function getBaseFare(): int{
+    return 30;
+  }
+
+  public function getPerkiloFare(): int{
+    return 3;
+  }
+}
+
+$car = new Car();
+$total = $car->getTotal(10);
+echo "Total fare for car: $total \n";
